5a4ce192fdd02da60544809ee0b416c04ffb3fcd
Filipe Dobreira <dobreira@gmail.com>
2015-06-03 19:05:07 +0100

    Simplify tooty_test.js

--- test/tooty/tooty_test.js
+++ test/tooty/tooty_test.js
@@ -4,18 +4,7 @@ import tooty from "../../lib/tooty";
 describe("tooty", () => {
   describe("#build", () => {
 
-    var router = tooty.build((r) => {
-      r.route("foo", null);
-      r.route("bar", null);
-
-      r.namespace("foo", (foo) => {
-        foo.route("boop", null);
-
-        foo.namespace("xx", (xx) => {
-          xx.route("zz", null);
-        });
-      });
-    });
+    var router = tooty.build(() => {});
 
     it("returns a router", () => {
       assert(router instanceof tooty.Router);
